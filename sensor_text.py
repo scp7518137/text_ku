7472f808cb9bd9a6e87101cb576c2224d7b48b67
scp7518137 <122028745+scp7518137@users.noreply.github.com>
2025-01-20 18:56:39 +0800

    text

--- sensor_text.py
+++ sensor_text.py
@@ -1,21 +1,13 @@
-import time
 from pyuptech import OnBoardSensors
 
-def main():
-    sensor_controller = OnBoardSensors()
-    sensor_controller.adc_io_open().set_all_io_mode(0)
-    while True:
-        try:
-            gpio_levels = sensor_controller.io_all_channels()
-            print(f"GPIO 引脚的电平状态（二进制）: {gpio_levels:08b}")
-            for i in range(8):
-                pin_level = (gpio_levels >> i) & 1
-                print(f"引脚 {i} 的电平: {'高' if pin_level else '低'}")
-            # 每隔 1 秒获取一次电平信息
-            time.sleep(1)
-        except Exception as e:
-            print(f"获取 GPIO 引脚电平时出现错误: {e}")
 
+def main():
+    # 创建OnBoardSensors对象
+    sensor_controller = OnBoardSensors()
+    # 初始化所有GPIO引脚为输入模式
+    sensor_controller.set_all_io_mode(0)
+    a = sensor_controller.get_all_io_mode()
+    print(a)
 
 if __name__ == "__main__":
     main()
